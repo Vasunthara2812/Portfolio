29d91212897fc94bca86a8c86309ea5f0c382b25
Vasunthara2812 <vasunthara28122001@gmail.com>
2025-10-19 22:07:56 +0530

    Add files via upload

---
+++ mywork_data.js
@@ -0,0 +1,37 @@
+import project_1 from '../assets/project_1.PNG'
+import project_2 from '../assets/project_2.PNG'
+import project_3 from '../assets/project_3.PNG'
+import project_4 from '../assets/project_4.PNG'
+import project_5 from '../assets/project_5.PNG'
+
+
+const mywork_data = [
+    {
+        w_no:1,
+        w_name:"Web design",
+        w_img:project_1
+    },
+    {
+        w_no:2,
+        w_name:"Web design",
+        w_img:project_2
+    },
+    {
+        w_no:3,
+        w_name:"Web design",
+        w_img:project_3
+    },
+    {
+        w_no:4,
+        w_name:"Web design",
+        w_img:project_4
+    },
+    {
+        w_no:5,
+        w_name:"Web design",
+        w_img:project_5
+    },
+   
+]
+ 
+export default mywork_data;
---
+++ services_data.js
@@ -0,0 +1,34 @@
+const Services_Data = [
+    {
+        s_no:"01",
+        s_name:"Data Cleaning & Preparation",
+        s_desc:"Transform raw data into clean, usable datasets using Excel, Python, or SQL."
+    },
+    {
+        s_no:"02",
+        s_name:"Data Analysis & Visualization",
+        s_desc:"Create insights and visualizations using tools like Excel, Power BI, Tableau, or Python libraries"
+    },
+    {
+        s_no:"03",
+        s_name:"Basic Statistical Analysis",
+        s_desc:"Perform descriptive statistics, correlations, and trend analysis"
+    },
+    {
+        s_no:"04",
+        s_name:"Reporting & Dashboards",
+        s_desc:"Design interactive dashboards and generate reports for data-driven decision-making."
+    },
+    {
+        s_no:"05",
+        s_name:"Data Entry & Data Management",
+        s_desc:"Efficient handling of datasets, databases, and maintaining data accuracy."
+    },
+    {
+        s_no:"06",
+        s_name:"Excel Automation / Python Scripting",
+        s_desc:"Automate repetitive tasks, calculations, and reporting using Excel or Python"
+    },
+]
+ 
+export default Services_Data;
